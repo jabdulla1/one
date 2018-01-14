afc71c4e62179e48be559f229904e1e221483aba
jabdulla1 <jihadabdullatif1@gmail.com>
2018-01-14 18:37:14 -0500

    bismillah commit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("bsmillahl please work");
+        System.out.println("bsmillahl please work is it working");
     }
 }
